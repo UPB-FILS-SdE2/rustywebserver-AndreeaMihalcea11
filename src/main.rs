2478962cf6e54d9f272659f0af9d8a9dff93513a
AndreeaMihalcea11 <161422716+AndreeaMihalcea11@users.noreply.github.com>
2024-06-24 11:47:27 +0000

    da

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::path::Path;
 use std::process::{Command, Stdio};
 use std::sync::Arc;
 use std::thread;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
